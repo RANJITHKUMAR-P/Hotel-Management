2f00422a6e15899cf37d6afa8fa7b569e3a66cfe
RANJITHKUMAR-P <ranjithkumar1772002@gmail.com>
2025-09-09 23:11:05 +0530

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ app.use(cors({
   origin: [
     "http://localhost:3000", 
     "http://localhost:5173",
-    "https://hotel-management-puce-tau.vercel.app", // ADD YOUR FRONTEND URL
+    "https://hotel-management-puce-tau.vercel.app/", // ADD YOUR FRONTEND URL
     "https://hotel-management-*.vercel.app"         // ADD WILDCARD FOR ALL VERCEL SUBDOMAINS
   ],
   credentials: true
